8e81e362c1ffb97fbf10ab6c92b1bda79ff18547
a1y2u3s4h <knownfacts23@gmail.com>
2026-07-12 21:32:09 +0530

    Update Swagger configuration

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,31 +1,29 @@
-const swaggerDocument = {
-  openapi: "3.0.0",
-  info: {
-    title: "Crypto Payment Gateway API",
-    version: "1.0.0",
-    description: "Backend API for Crypto Payment Gateway",
-  },
-  servers: [
-  {
-    url:
-      process.env.NODE_ENV === "production"
-        ? "https://crypto-payment-gateway-0gp5.onrender.com"
-        : "http://localhost:5000",
-  },
-],
-  
-  paths: {
-    "/api/v1/health": {
-      get: {
-        summary: "Health Check",
-        responses: {
-          "200": {
-            description: "Server Running",
-          },
-        },
-      },
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Crypto Payment Gateway API",
+      version: "1.0.0",
+      description: "Backend API for Crypto Payment Gateway",
     },
+    servers: [
+      {
+        url:
+          process.env.NODE_ENV === "production"
+            ? "https://crypto-payment-gateway-0gp5.onrender.com"
+            : "http://localhost:5000",
+      },
+    ],
   },
+
+  apis: [
+    "./src/modules/**/*.ts",
+    "./src/routes/*.ts"
+  ],
 };
 
-export default swaggerDocument;
+const swaggerSpec = swaggerJsdoc(options);
+
+export default swaggerSpec;
